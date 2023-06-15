66084448c017ab496733a9a88b72877f94b65453
aran085 <ARAN-085@proton.me>
2023-06-15 07:33:42 +0000

    Setting version to 1.0

---
+++ src/main/java/rainbownlp/analyzer/evaluation/FeatureEvaluator.java
@@ -0,0 +1,13 @@
+package rainbownlp.analyzer.evaluation;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import rainbownlp.core.FeatureValuePair;
+import rainbownlp.machinelearning.MLExample;
+import rainbownlp.machinelearning.convertor.SVMLightFormatConvertor;
+
+public class FeatureEvaluator {
+	public void evaluateFeatures(ICrossfoldValidator cfValidator, List<MLExample> examples) throws Exception{
+		SVMLightFormatC
