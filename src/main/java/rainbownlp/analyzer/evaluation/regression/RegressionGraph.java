4398c8b9775a0ad0bba15066421b1363a6ccf0ee
aran085 <ARAN-085@proton.me>
2023-06-26 15:13:23 +0000

    New .pot files for 2.9

---
+++ src/main/java/rainbownlp/analyzer/evaluation/regression/RegressionGraph.java
@@ -0,0 +1,17 @@
+package rainbownlp.analyzer.evaluation.regression;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.RenderingHints;
+import java.awt.Stroke;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class RegressionGraph extends JPanel {
+	   pr
