18037a2efaf3f100a33f7092c17477dd825217cd
aran085 <ARAN-085@proton.me>
2023-07-20 10:43:11 +0000

    Fixed namespace

--- src/main/java/rainbownlp/cloud/amazon/emr/AmazonEMRManager.java
+++ src/main/java/rainbownlp/cloud/amazon/emr/AmazonEMRManager.java
@@ -1,1 +1,12 @@
-package rainb
+package rainbownlp.cloud.amazon.emr;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduce;
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduceClient;
+import com.amazonaws.services.elasticmapreduce.model.AddJobFlowStepsRequest;
+import com.amazonaws.services.elasticmapreduce.model.AddJobFlowSteps
