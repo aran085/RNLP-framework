41b5ce7879efab0fed0f0559f5bf59bb72e88823
aran085 <ARAN-085@proton.me>
2023-07-20 15:57:51 +0000

    Improve static analysis

--- src/main/java/rainbownlp/cloud/amazon/emr/AmazonEMRManager.java
+++ src/main/java/rainbownlp/cloud/amazon/emr/AmazonEMRManager.java
@@ -20,4 +20,19 @@ public class AmazonEMRManager {
 		try {
 			credentials = new PropertiesCredentials(
 					AmazonEMRManager.class.getResourceAsStream("AwsCredentials.properties"));
-		} catch (IOE
+		} catch (IOException e1) {
+			System.out.println("Credentials were not properly entered into AwsCredentials.properties.");
+			System.out.println(e1.getMessage());
+			System.exit(-1);
+		}
+
+		AmazonElasticMapReduce client = new AmazonElasticMapReduceClient(credentials);
+
+		List<StepConfig> stepsConfig = new ArrayList<StepConfig>();
+		int counter = 0;
+		for(HadoopJarStepConfig step : steps){
+			counter++;
+			stepsConfig.add(new StepConfig("Step"+counter, step));
+		}
+
+		A
