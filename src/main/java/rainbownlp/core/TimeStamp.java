c439f827e780e842ef9690c3019c3dc67668f2fb
aran085 <ARAN-085@proton.me>
2023-08-15 21:17:39 +0000

    change variables

--- src/main/java/rainbownlp/core/TimeStamp.java
+++ src/main/java/rainbownlp/core/TimeStamp.java
@@ -9,4 +9,15 @@ public class TimeStamp {
 		blockTimes.put(blockName, new Date());
 	}
 
-	public static void setEnd(String blockNam
+	public static void setEnd(String blockName) {
+		Date now = new Date();
+		Date start = blockTimes.get(blockName);
+		if(start!=null){
+			Double timePassed = (double)(now.getTime() - start.getTime())/(double)1000;
+			if(timePassed>1)
+			System.out.println("TIME - "+blockName+" : "+timePassed);
+		}
+		
+	}
+
+}
---
+++ src/main/java/rainbownlp/core/graph/GraphEdge.java
@@ -0,0 +1,9 @@
+package rainbownlp.core.graph;
+
+import org.jgrapht.graph.DefaultEdge;
+
+public class GraphEdge extends DefaultEdge {
+	/**
+	 * 
+	 */
+	private static fin
