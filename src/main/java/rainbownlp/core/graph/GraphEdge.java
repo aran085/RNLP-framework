a33a9b42a23d87375ffa55fb87c83e4065f024a0
aran085 <ARAN-085@proton.me>
2023-08-16 13:28:18 +0000

    old code

--- src/main/java/rainbownlp/core/graph/GraphEdge.java
+++ src/main/java/rainbownlp/core/graph/GraphEdge.java
@@ -6,4 +6,22 @@ public class GraphEdge extends DefaultEdge {
 	/**
 	 * 
 	 */
-	private static fin
+	private static final long serialVersionUID = 1L;
+	public GraphEdge()
+	{
+		super();
+	}
+	
+    //~ Methods ----------------------------------------------------------------
+
+//	@Override
+	public Object getSource()
+	{
+		//"(" + source + " : " + target + ")"
+		return super.toString().split(":")[0].replaceAll("^\\(", "");
+	}
+//	@Override
+	public Object getTarget()
+	{
+		//"(" + source + " : " + target + ")"
+		return super.toString().split(":")[1].replaceAll("\\)$", ""
