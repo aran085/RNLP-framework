526e0da5e34352fe751a0baec2af03f17a27877a
aran085 <ARAN-085@proton.me>
2023-08-19 18:57:36 +0000

    move docs

--- src/main/java/rainbownlp/machinelearning/MLExample.java
+++ src/main/java/rainbownlp/machinelearning/MLExample.java
@@ -1,4 +1,19 @@
 package rainbownlp.machinelearning;
 
 import java.io.Serializable;
-import java.
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import 
