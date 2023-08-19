2e14de179d453d9d46c603ca4fc0343e1a61d49f
aran085 <ARAN-085@proton.me>
2023-08-19 05:59:26 +0000

    Fix failed tests

--- src/main/java/rainbownlp/machinelearning/LearnerCommon.java
+++ src/main/java/rainbownlp/machinelearning/LearnerCommon.java
@@ -3,4 +3,23 @@ package rainbownlp.machinelearning;
 import java.sql.SQLException;
 
 import rainbownlp.util.ConfigurationUtil;
-import rainbownlp.util
+import rainbownlp.util.ConfigurationUtil.OperationMode;
+
+public class LearnerCommon {
+
+	public static void includeExamples(String updateTo) throws SQLException {
+//		if(Setting.Mode==OperationMode.EDGE)
+//		{
+//			RelationExampleTable.setTestAsTrain();
+//			RelationExampleTable.include(updateTo);
+//		}
+//		if(Setting.Mode==OperationMode.TRIGGER)
+//		{
+//			ArtifactExampleTable.setTestAsTrain();
+//			ArtifactExampleTable.include(updateTo);
+//		}
+	}
+
+	public static String[] getClassTitles() {
+		String[] class_titles = new String[1];
+	
