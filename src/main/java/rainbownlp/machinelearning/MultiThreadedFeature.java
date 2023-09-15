8839c27d693f610559c36bf513e46cb955cade79
aran085 <ARAN-085@proton.me>
2023-09-15 17:56:36 +0000

    change location

--- src/main/java/rainbownlp/machinelearning/MultiThreadedFeature.java
+++ src/main/java/rainbownlp/machinelearning/MultiThreadedFeature.java
@@ -5,4 +5,19 @@ import java.util.List;
 
 import rainbownlp.util.FileUtil;
 
-public
+public abstract class MultiThreadedFeature extends Thread implements IFeatureCalculator {
+	protected List<MLExample> examples;
+	public MultiThreadedFeature(List<MLExample> examplesToProcess){
+		examples = examplesToProcess;
+	}
+	@Override
+	public abstract void calculateFeatures(MLExample exampleToProcess)
+			throws SQLException, Exception;
+
+	@Override
+	public void run() {
+		int counter = 0;
+		for (MLExample example:examples)
+		{
+			try {
+				calculateFeatures(e
