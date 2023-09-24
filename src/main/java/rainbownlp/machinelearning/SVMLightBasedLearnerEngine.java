6069b4280040107a7aaa6acc950c098fe18e3232
aran085 <ARAN-085@proton.me>
2023-09-24 10:03:10 +0000

    update linting

--- src/main/java/rainbownlp/machinelearning/SVMLightBasedLearnerEngine.java
+++ src/main/java/rainbownlp/machinelearning/SVMLightBasedLearnerEngine.java
@@ -8,4 +8,19 @@ import java.util.List;
 
 import rainbownlp.machinelearning.convertor.SVMLightFormatConvertor;
 import rainbownlp.util.ConfigurationUtil;
-import rainbownlp.util.HibernateUtil
+import rainbownlp.util.HibernateUtil;
+import rainbownlp.util.SystemUtil;
+
+
+public abstract class SVMLightBasedLearnerEngine extends LearnerEngine {
+
+	
+	public void train(List<MLExample> pTrainExamples) throws Exception {
+		ConfigurationUtil.TrainingMode = true;
+		HibernateUtil.clearLoaderSession();
+		
+		//This part added since the session was so slow
+		List<Integer> train_example_ids = new ArrayList<Integer>();
+		for(MLExample example : pTrainExamples)
+		{
+			train_example_i
