b1b783152f98e75c175ef31c0f24cb343b593bc3
aran085 <ARAN-085@proton.me>
2023-09-25 16:00:06 +0000

    update README.txt

--- src/main/java/rainbownlp/machinelearning/convertor/WekaFormatConvertor.java
+++ src/main/java/rainbownlp/machinelearning/convertor/WekaFormatConvertor.java
@@ -6,4 +6,16 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
-import rainbownlp.core
+import rainbownlp.core.FeatureValuePair;
+import rainbownlp.machinelearning.MLExample;
+import rainbownlp.machinelearning.MLExampleFeature;
+import rainbownlp.util.FileUtil;
+import rainbownlp.util.HibernateUtil;
+import rainbownlp.util.ConfigurationUtil;
+import weka.core.Attribute;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.SparseInstance;
+import weka.core.converters.ArffSaver;
+import weka.core.conve
