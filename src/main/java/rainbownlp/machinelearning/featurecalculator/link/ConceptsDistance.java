206424af2081a96b76ab506d456b8e3c5274af41
aran085 <ARAN-085@proton.me>
2023-10-05 15:32:38 +0000

    update photos

--- src/main/java/rainbownlp/machinelearning/featurecalculator/link/ConceptsDistance.java
+++ src/main/java/rainbownlp/machinelearning/featurecalculator/link/ConceptsDistance.java
@@ -1,4 +1,20 @@
 /**
  * 
  */
-pack
+package rainbownlp.machinelearning.featurecalculator.link;
+
+import java.util.List;
+import rainbownlp.machinelearning.IFeatureCalculator;
+import rainbownlp.machinelearning.MLExample;
+import rainbownlp.util.FileUtil;
+
+/**
+ * @author ehsan
+ * 
+ */
+public class ConceptsDistance implements IFeatureCalculator {
+	public static void main (String[] args) throws Exception
+	{
+		String experimentgroup = "LinkClassificationEventEvent";
+		List<MLExample> trainExamples = 
+			MLExample
