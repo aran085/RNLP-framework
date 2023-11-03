cdedb48ad4ab506144df2590c759e9da1fd96bf0
aran085 <ARAN-085@proton.me>
2023-11-03 18:53:50 +0000

    Increased version

--- src/main/java/rainbownlp/machinelearning/featurecalculator/link/ParseTreeFeatures.java
+++ src/main/java/rainbownlp/machinelearning/featurecalculator/link/ParseTreeFeatures.java
@@ -3,4 +3,21 @@
  */
 package rainbownlp.machinelearning.featurecalculator.link;
 
-import j
+import java.util.List;
+
+import rainbownlp.core.Artifact;
+import rainbownlp.core.FeatureValuePair;
+import rainbownlp.core.Phrase;
+import rainbownlp.core.PhraseLink;
+import rainbownlp.core.FeatureValuePair.FeatureName;
+import rainbownlp.core.graph.GraphEdge;
+import rainbownlp.machinelearning.IFeatureCalculator;
+import rainbownlp.machinelearning.MLExample;
+import rainbownlp.machinelearning.MLExampleFeature;
+import rainbownlp.parser.DependenciesTreeUtil;
+
+/**
+ * @author ehsan
+ * 
+ */
+public class ParseTreeFeatu
