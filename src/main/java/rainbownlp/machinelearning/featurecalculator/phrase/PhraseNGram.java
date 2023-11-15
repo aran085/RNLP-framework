b95ad5a3f7f54eb3cc54b0479c4fbce23a93ca60
aran085 <ARAN-085@proton.me>
2023-11-15 17:04:39 +0000

    Update skills.html

--- src/main/java/rainbownlp/machinelearning/featurecalculator/phrase/PhraseNGram.java
+++ src/main/java/rainbownlp/machinelearning/featurecalculator/phrase/PhraseNGram.java
@@ -1,1 +1,13 @@
-package rainbownlp.machinelearning.f
+package rainbownlp.machinelearning.featurecalculator.phrase;
+
+import java.util.List;
+
+import rainbownlp.core.Artifact;
+import rainbownlp.core.Artifact.Type;
+import rainbownlp.core.FeatureValuePair;
+import rainbownlp.core.Phrase;
+import rainbownlp.core.PhraseLink;
+import rainbownlp.machinelearning.IFeatureCalculator;
+import rainbownlp.machinelearning.MLExample;
+import rainbownlp.machinelearning.MLExampleFeature;
+import rainbownlp.machinelearning.featurecalcu
