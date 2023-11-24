96b5c9aaff9a9507053e008081fb514a14db392b
aran085 <ARAN-085@proton.me>
2023-11-24 11:54:15 +0000

    for v0.0.1

--- src/main/java/rainbownlp/machinelearning/featurecalculator/sentence/SentenceSyntax.java
+++ src/main/java/rainbownlp/machinelearning/featurecalculator/sentence/SentenceSyntax.java
@@ -1,1 +1,14 @@
-package rainbownlp.mac
+package rainbownlp.machinelearning.featurecalculator.sentence;
+
+import rainbownlp.core.Artifact;
+import rainbownlp.core.FeatureValuePair;
+import rainbownlp.core.Artifact.Type;
+import rainbownlp.machinelearning.IFeatureCalculator;
+import rainbownlp.machinelearning.MLExample;
+import rainbownlp.machinelearning.MLExampleFeature;
+
+public class SentenceSyntax implements IFeatureCalculator {
+
+	@Override
+	public void calculateFeatures(MLExample exampleToProcess) {
+		Artifact sentence = exampleToProce
