c4400f89db25ff6ada58ef7a986a5a8d421b3f49
aran085 <ARAN-085@proton.me>
2023-12-20 19:58:09 +0000

    add condition

---
+++ src/main/java/rainbownlp/tests/unit/core/ArtifactTest.java
@@ -0,0 +1,34 @@
+
+package rainbownlp.tests.unit.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import rainbownlp.core.Artifact;
+import rainbownlp.core.Artifact.Type;
+import rainbownlp.util.HibernateUtil;
+
+public class ArtifactTest   {
+	
+	@Test
+	public void testCreateArtifact() {
+		Artifact doc_artifact = Artifact.getInstance(Type.Document);
+		assertNotNull(doc_artifact);
+
+		doc_artifact.setContent( "this is test. hello test.");
+		
+		HibernateUtil.save(doc_artifact);
+
+		assertEquals(doc_artifact.getContent(), "this is test. hello test.");
+		assertEquals(doc_artifact.getArtifactType(), Type.Document);
+		assertTrue(doc_artifact.getArtifactId()!=-1);
+		
+		Artifact sentence_artifact = Artifact.getInstance(Type.Sentence);
+		sentence_artifact.setContent("this is test.");
+		sentence_artifact.setParentArtifact(doc_artifact);
+		
+		HibernateUtil.save(sentence_artifact);
+		
+		assertEquals(sentence_artifact.getContent(), "this is test.");
+		assertEquals(sentence_artifact.getArtifactType(), Type.Sentence);
