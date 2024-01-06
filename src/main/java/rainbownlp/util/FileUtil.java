ba6ed0d82834cd9e1231cecd196493ca5f217bab
aran085 <ARAN-085@proton.me>
2024-01-06 19:43:51 +0000

    minor..

--- src/main/java/rainbownlp/util/FileUtil.java
+++ src/main/java/rainbownlp/util/FileUtil.java
@@ -222,3 +222,160 @@ public class FileUtil {
 	 * @param filePath
 	 * @return The File content
 	 * @throws IOException
+	 */
+	public static String readWholeFile(String filePath) throws IOException
+	{
+		StringBuffer fileData = new StringBuffer(1000);
+		FileReader fr = new FileReader(filePath);
+        BufferedReader reader = new BufferedReader(fr);
+        char[] buf = new char[1024];
+        int numRead=0;
+        while((numRead=reader.read(buf)) != -1){
+            String readData = String.valueOf(buf, 0, numRead);
+            fileData.append(readData);
+            buf = new char[1024];
+        }
+        reader.close();
+        fr.close();
+        return fileData.toString();
+	}
+	
+	public static List<File> getFilesInDirectory(String root,String extension)
+	{
+		List<File> fileNames = new ArrayList<>();
+		try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get(root))) {
+            for (Path path : directoryStream) {
+            	if(path.toString().endsWith(extension))
+            		fileNames.add(new File(path.toString()));
+            }
+        } catch (IOException ex) {}
+        
+        return fileNames;
+	}
+	public static List<String> loadLineByLine(String file_path) {
+		return loadLineByLine(new File(file_path));
+	}
+	public static List<String> loadLineByLine(File f) {
+		List<String> lines = new ArrayList<String>();
+		try {
+			if(!f.exists()) return lines;
+			BufferedReader br1 = new BufferedReader(
+					new FileReader(f));
+			while (br1.ready()) {
+				String line = br1.readLine();
+				lines.add(line);
+			}
+			br1.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return lines;
+	}
+	public static List<String> loadLineByLineAndTrim(String file_path) {
+		List<String> lines = new ArrayList<String>();
+		try {
+			File f = new File(file_path);
+			if(!f.exists()) return lines;
+			BufferedReader br1 = new BufferedReader(
+					new FileReader(f));
+			while (br1.ready()) {
+				String line = br1.readLine();
+				lines.add(line.trim());
+			}
+			br1.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return lines;
+	}
+
+
+	public static boolean fileExists(String path) {
+		File file = new File(path);
+		return file.exists();
+	}
+	public static String getFilePathInDirectory(String directory, String fileName)
+	{
+		String file_path = directory;
+		file_path += "/"+fileName;
+		return file_path;
+	}
+	public static void logLine(String filePath, String line)
+	{
+		if(filePath==null)
+		{
+			String log = (new Date())+" : "+line+"\n";
+			System.out.print(log);
+		}else
+		{
+			try {
+				
+				BufferedWriter writer = new BufferedWriter( new FileWriter( filePath , true ) );
+//				writer.write((new Date())+" : "+line+"\n");
+				writer.write(line+"\n");
+				writer.flush();
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public static String ReadFileInToString(String filePath) throws IOException {
+	       
+        //create file object
+        File file = new File(filePath);
+        BufferedInputStream bin = null;
+        String strFileContents = null;
+        StringBuffer fileData = new StringBuffer();
+        BufferedReader reader = new BufferedReader(
+                new FileReader(filePath));
+        char[] buf = new char[1024];
+        int numRead=0;
+        while((numRead=reader.read(buf)) != -1){
+            String readData = String.valueOf(buf, 0, numRead);
+            fileData.append(readData);
+        }
+        reader.close();
+        return fileData.toString();
+	}
+	
+	public static String getTextFileName(String FilePath)
+	{
+		String file_name= getFileNameWithoutPath(FilePath);
+		String text_file = "/"+file_name+".txt";
+		return text_file;
+	}
+	public static String getFileNameWithoutPath(String FilePath)
+	{
+		String file_name="";
+		Pattern p = Pattern.compile("(.*/)(.+)\\.\\w\\w\\w.?(txt)?$");
+		Matcher m = p.matcher(FilePath);
+		if (m.matches())
+		{
+			file_name = m.group(2);
+		}
+		return file_name;
+	}
+	
+	public static Document parseXmlFile(String xmlFile) throws Exception{
+		//get the factory
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+
+		//Using factory get an instance of document builder
+		DocumentBuilder db = dbf.newDocumentBuilder();
+
+		//parse using builder to get DOM representation of the XML file
+		Document dom = db.parse(xmlFile);
+
+
+		return dom;
+	}
+
+	public static void logLine(String line) {
+		logLine(null, line);
+	}
+
+ 
+
+}
---
+++ src/main/java/rainbownlp/util/PorterStemmer.java
@@ -0,0 +1,89 @@
+
+package rainbownlp.util;
+
+
+/*
+
+   Porter stemmer in Java. The original paper is in
+
+       Porter, 1980, An algorithm for suffix stripping, Program, Vol. 14,
+       no. 3, pp 130-137,
+
+   See also http://www.tartarus.org/~martin/PorterStemmer
+
+   History:
+
+   Release 1
+
+   Bug 1 (reported by Gonzalo Parra 16/10/99) fixed as marked below.
+   The words 'aed', 'eed', 'oed' leave k at 'a' for step 3, and b[k-1]
+   is then out outside the bounds of b.
+
+   Release 2
+
+   Similarly,
+
+   Bug 2 (reported by Steve Dyrdahl 22/2/00) fixed as marked below.
+   'ion' by itself leaves j = -1 in the test for 'ion' in step 5, and
+   b[j] is then outside the bounds of b.
+
+   Release 3
+
+   Considerably revised 4/9/00 in the light of many helpful suggestions
+   from Brian Goetz of Quiotix Corporation (brian@quiotix.com).
+
+   Release 4
+
+*/
+
+import java.io.*;
+
+/**
+  * Stemmer, implementing the Porter Stemming Algorithm
+  *
+  * The Stemmer class transforms a word into its root form.  The input
+  * word can be provided a character at time (by calling add()), or at once
+  * by calling one of the various stem(something) methods.
+  */
+
+public class PorterStemmer
+{  private char[] b;
+   private int i,     /* offset into b */
+               i_end, /* offset to end of stemmed word */
+               j, k;
+   private static final int INC = 50;
+                     /* unit of size whereby b is increased */
+   public PorterStemmer()
+   {  b = new char[INC];
+      i = 0;
+      i_end = 0;
+   }
+
+   /**
+    * Add a character to the word being stemmed.  When you are finished
+    * adding characters, you can call stem(void) to stem the word.
+    */
+
+   public void add(char ch)
+   {  if (i == b.length)
+      {  char[] new_b = new char[i+INC];
+         for (int c = 0; c < i; c++) new_b[c] = b[c];
+         b = new_b;
+      }
+      b[i++] = ch;
+   }
+
+
+   /** Adds wLen characters to the word being stemmed contained in a portion
+    * of a char[] array. This is like repeated calls of add(char ch), but
+    * faster.
+    */
+
+   public void add(char[] w, int wLen)
+   {  if (i+wLen >= b.length)
+      {  char[] new_b = new char[i+wLen+INC];
+         for (int c = 0; c < i; c++) new_b[c] = b[c];
+         b = new_b;
+      }
+      for (int c = 0; c < wLen; c++) b[i++] = w[c];
+   }
